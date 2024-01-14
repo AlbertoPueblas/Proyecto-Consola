2c7d49aca476d5ae531f305a279578efda3ba19b
Alberto <alberto.c.pueblas@gmail.com>
2024-01-14 12:41:29 +0100

    Creando html y css y dando forma a la consola

--- js/main.js
+++ js/main.js
@@ -1,2 +1,28 @@
-console.log("Hola Mundo");
-alert("Hola Java");
+//Data definition
+var number1 = +prompt("Ingrese number1");
+var number2 = +prompt("Ingrese number2");
+
+var sum, subtraction, multiplication, division;
+
+//Calculate sum
+sum = number1 + number2;
+
+//Calculate substracion
+
+subtraction = number1 - number2;
+
+//Calculate multiplication
+
+multiplication = number1 * number2;
+
+//Calculate division
+
+division = number1 / number2;
+
+//show result
+
+console.log("sum:" + sum);
+console.log("subtracion:" + subtraction)
+console.log("mutiplication" + multiplication)
+console.log("division" + division)
+
