9f4f677c63f6654a2ab2524909155747da5b5828
Alberto <alberto.c.pueblas@gmail.com>
2024-01-15 23:10:47 +0100

    modificacion JS boton de encendido

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+var botonStart = document.querySelector("#inicioConsola");
+
+var divLuz = document.querySelector("#ledRojo");
+
+var pantalla = document.querySelector("#pantalla");
+var encendido = false;
+
+
+botonStart.addEventListener("click",()=>{
+  if(encendido == true){
+    encendido = true;
+    divLuz.classList.toggle("PowerOff");
+    divLuz.classList.toggle("PowerOn");
+
+  }else {
+    encendido = false;
+    divLuz.classList.toggle("PowerOff");
+    divLuz.classList.toggle("PowerOn");
+
+  }
+})
 
