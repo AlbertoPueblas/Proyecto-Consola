3b5b7748e00b065c2d35d81fe62a6c649d5ab168
Alberto <alberto.c.pueblas@gmail.com>
2024-01-17 23:35:09 +0100

    cambios en la estructura de JS

--- js/main.js
+++ js/main.js
@@ -4,20 +4,52 @@ var divLuz = document.querySelector("#ledRojo");
 
 var pantalla = document.querySelector("#pantalla");
 
-var encendido = false;
+var sonido= document.querySelector("#sonido")
 
+var intro = new Audio();
+intro.src ="sonido/gameboy.mp3"
+
+var encendido = true;
+
+//Boton on/off//
 
 botonStart.addEventListener("click",()=>{
   if(encendido == true){
     encendido = true;
-    divLuz.classList.toggle("PowerOff");
     divLuz.classList.toggle("PowerOn");
+    pantalla.classList.toggle("idPantallaOn");
+    intro.play();
+    
 
-  }else {
+
+
+  }else if(encendido ==false) {
     encendido = false;
     divLuz.classList.toggle("PowerOff");
-    divLuz.classList.toggle("PowerOn");
+    pantalla.classList.toggle("idPantallaOff");
 
+
+}})
+
+
+var cambiarImg = document.querySelector("#derecha");
+
+var imagenConsola =document.getElementsByClassName("imagenConsola")
+
+var img1= '<img src="imagen/zelda.jpg" alt="" class="imagenConsola"></img>';
+
+var img2= '<img src="imagen/pokemon.jpg" alt="" class="imagenConsola"></img>';
+
+var img3= '<img src="imagen/mario.jpg" alt="" class="imagenConsola"></img>';
+
+var pantalla = document.querySelector("#pantalla");
+
+var imagenes = [img1,img2,img3];
+
+cambiarImg.addEventListener("click", function () {
+  pantalla.backgroundImagen = imagenes[img1,img2,img3];
+
+  if (imagenes < backgroundImagen) {
+     imagenes++;
   }
-})
-
+});
