7872c4cf79856c353d13aaece1db28c83ecf7e0b
Alberto <alberto.c.pueblas@gmail.com>
2024-01-18 17:58:16 +0100

    creacion boton Reset y solucion de errores

--- js/main.js
+++ js/main.js
@@ -4,41 +4,41 @@ var encendido=false;
 var cambio = document.getElementById("derecha");
 var cambioIz= document.getElementById("izquierda");
 var reset = document.getElementById("reset");
-var img1 = "url(./imagen/gbc.gif)";
-var img2 = "url(./imagen/pokemon.jpg)";
-var img3 = "url(./imagen/Mario.jpg)";
-var img4 = "url(./imagen/supermario.jpg)";
+//importacion de imagenes
+var imgOn = "url(./imagen/gbc.gif)";
+var img1 = "url(./imagen/zelda.jpg)";
+var img2 = "url(./imagen/Mario.jpg)";
+var img3 = "url(./imagen/supermario.jpg)";
 var imgOff = "url(./imagen/fondogris.jpg)";
-var imagenes=[img1,img2,img3,img4];
-var posicionArray = 0;
+var imagenes=[img1,img2,img3,];
 
+var posicionArray = 0;
+var powerOn= document.getElementById("ledRojo")
+
+//importacion de audios
 var intro= new Audio();
 intro.src="./sonido/gameboy.mp3"
-
 var soundReset= new Audio();
 soundReset.src="./sonido/pacman.mp3"
-
 var flechasSound= new Audio();
 flechasSound.src="./sonido/marioCoin.mp3"
 
 
+
+//Botón de encendido
 botonEncender.addEventListener("click",function(){
   if(!encendido){
     encendido = true;
-    
-
-    pantalla.style.backgroundImage=imagenes[0];
-    posicionArray=1;
+    pantalla.style.backgroundImage=imgOn;
+    powerOn.classList.toggle("PowerOn")
     intro.play();
-
+    
   }else if(encendido){
     encendido=false;
     pantalla.style.backgroundImage=imgOff;
-    
-
-  }
-})
-
+    powerOn.classList.toggle("PowerOn")
+}})
+//Cambio imagenes botón derecha
 cambio.addEventListener("click",function(){
   if(encendido){
     flechasSound.play();
@@ -60,7 +60,7 @@ cambio.addEventListener("click",function(){
   }
 }
 })
-
+//cambio imagenes botón izquierdo
 cambioIz.addEventListener("click",function(){
   if(encendido){
     flechasSound.play();
@@ -76,11 +76,12 @@ cambioIz.addEventListener("click",function(){
     posicionArray=0;
   }
 }})
-
+//boton reset
 reset.addEventListener("click",()=>{
-  pantalla.style.backgroundImage=imagenes[0];
-  posicionArray=1;
+ if(encendido){
+  pantalla.style.backgroundImage=imgOn;
   soundReset.play();
+ }
 
   
 })
